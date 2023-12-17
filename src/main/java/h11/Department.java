32c6e8e4794d1277d0f6d99cdade748387ea4f16
LWcoder02 <larsw02@gmail.com>
2023-12-17 12:16:01 +0100

    Added Department class

---
+++ src/main/java/h11/Department.java
@@ -0,0 +1,17 @@
+package h11;
+
+import java.util.List;
+
+public class Department {
+
+    private List<Employee> employees;
+
+    public Department(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+
+    public List<Job> getListOfJobsInDepartment() {
+        return null;
+    }
+}
