398500fc4e010bd649ab9f6d5cbf19f255a7b2a1
LWcoder02 <larsw02@gmail.com>
2023-12-18 15:40:03 +0100

    Added methods

--- src/main/java/h11/Company.java
+++ src/main/java/h11/Company.java
@@ -1,5 +1,6 @@
 package h11;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -19,15 +20,14 @@ public class Company {
     public List<Employee> getListOfAllEmployee() {
         return this.departments.stream()
             .flatMap(d -> d.getEmployees().stream())
-            .collect(Collectors.toList());
+            .toList();
     }
 
-    public int getQuantityOfProduct(Product product) {
+    public long getQuantityOfProduct(Product product) {
         return this.warehouses.stream()
             .flatMap(warehouse -> warehouse.getProducts().stream())
             .filter(p -> p == product)
-            .collect(Collectors.toList())
-            .size();
+            .count();
     }
 
     public List<String> getFilteredProductNames(List<Predicate<Product>> predicates) {
@@ -37,5 +37,38 @@ public class Company {
             .map(Product::getName)
             .toList();
     }
+
+    public List<Product> priceRange(double low, double high) {
+        return this.warehouses.stream()
+            .flatMap(warehouse -> warehouse.getProducts().stream())
+            .filter(p -> p.getPrice() < high && p.getPrice() > low)
+            .sorted(Comparator.comparingDouble(Product::getPrice))
+            .toList();
+    }
+
+    public List<String> getEmployeesSortedByName() {
+        return this.departments.stream()
+            .flatMap(department -> department.getEmployees().stream())
+            .map(employee -> employee.getName())
+            .map(name -> {
+                String[] splittedName = name.split(" ");
+                return splittedName[1] + ", "+ splittedName[0];
+            })
+            .toList();
+    }
+
+    public List<String> getAllProductsByType(ProductTyp type) {
+        return this.warehouses.stream()
+            .flatMap(warehouse -> warehouse.getProducts().stream())
+            .filter(product -> product.getProductTyp().equals(type))
+            .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+            .limit(5)
+            .map(product -> String.format("%s: %f€", product.getName(), product.getPrice()))
+            .toList();
+
+        // {Laptop: 1000$, ()}
+    }
+
+
 
 }
