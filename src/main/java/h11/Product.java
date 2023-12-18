9809a030d93bb7fe9a8041362f88683c22af81d4
LWcoder02 <larsw02@gmail.com>
2023-12-18 09:29:42 +0100

    Added setter methods and new constant "PRODUCT_NAME"

--- src/main/java/h11/Product.java
+++ src/main/java/h11/Product.java
@@ -5,16 +5,30 @@ public class Product {
     private final ProductTyp TYP;
     private int price;
     private int quantity;
+    private final String PRODUCT_NAME;
 
-    public Product(ProductTyp typ, int price, int quantity) {
+    public Product(ProductTyp typ, int price, int quantity, String productName) {
         this.TYP = typ;
         this.price = price;
         this.quantity = quantity;
+        this.PRODUCT_NAME = productName;
     }
 
 
     public int getPrice() {
         return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     public ProductTyp getProductTyp() {
